4042926f9d0829163ee578bc91e4254c1e70b1e8
chrisyuen976 <chrisyuen976@gmail.com>
2023-09-16 20:18:49 +1000

    moveboat function at timeinterval

--- src/components/replay/Replay.jsx
+++ src/components/replay/Replay.jsx
@@ -6,20 +6,24 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { Water } from "three/addons/objects/Water.js";
 import { Sky } from "three/addons/objects/Sky.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import data from '../../data/direction.json'
+import data from "../../data/direction.json";
 import waternormals from "../../../public/assets/waternormals.jpg";
+import timeAndXYData from "../../data/timeAndXY.json";
 
 let camera, scene, renderer;
-let controls, water, sun;
+let controls, water, sun, boat, northIndicator;
+let timeIntervals = [];
+let tempCube;
+
 const loader = new GLTFLoader();
 
-class Boat {
-  constructor() {
-    let boat_position = new THREE.Vector3(0, 0, 0);
+class Boat extends THREE.Group {
+  constructor(x, y) {
+    super();
     loader.load("../public/assets/boat/scene.gltf", (gltf) => {
       scene.add(gltf.scene);
       gltf.scene.scale.set(2, 2, 2);
-      gltf.scene.position.set(-17.3285, 1, -29.7828);
+      gltf.scene.position.set(x, 1, y);
       gltf.scene.rotation.y = -1.5;
 
       this.boat = gltf.scene;
@@ -44,23 +48,20 @@ class Boat {
       // console.log(this.boat.position);
     }
   }
-
-  position() {
-    return this.boat.position;
-  }
 }
 
 const Replay = ({ canvasRef, upperHalfRef, mapRef }) => {
   useEffect(() => {
     // Access and use the ref in the child component
-    console.log(canvasRef);
     if (upperHalfRef.current) {
       init();
     }
   }, []);
 
   function init() {
-    const boat = new Boat();
+    //Temporary cube
+
+    boat = new Boat(timeAndXYData[1].X_Position, timeAndXYData[1].Y_Position);
     // Create the WebGL renderer
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -76,12 +77,8 @@ const Replay = ({ canvasRef, upperHalfRef, mapRef }) => {
     renderer.domElement.style.zIndex = "-1";
     renderer.domElement.style.left = "0";
     renderer.domElement.style.top = "0";
-    // const renderDiv = document.getElementById("3d");
-    // console.log(renderDiv);
 
     upperHalfRef.current.appendChild(renderer.domElement);
-
-    // document.body.appendChild(renderer.domElement);
 
     // Create the scene
     scene = new THREE.Scene();
@@ -169,40 +166,54 @@ const Replay = ({ canvasRef, upperHalfRef, mapRef }) => {
     // Add event listener for window resize
     window.addEventListener("resize", onWindowResize);
 
+    // ===== Norh Indicator ======
     const cubeSize = 20;
     const indicatorDistance = 200; // Adjust the distance from the center
 
     const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
     const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const northIndicator = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    northIndicator = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
     // Position the cube at the north direction
     northIndicator.position.set(0, 50, indicatorDistance);
     scene.add(northIndicator);
 
+    // ===== Temp Cube ======
+    const tempCubeSize = 3;
 
-    // load data and create map
+    const tempCubeGeometry = new THREE.BoxGeometry(
+      tempCubeSize,
+      tempCubeSize,
+      tempCubeSize * 2
+    );
+    const tempCubeMaterial = new THREE.MeshBasicMaterial({ color: 0x800080 });
+    tempCube = new THREE.Mesh(tempCubeGeometry, tempCubeMaterial);
+
+    // Position the cube at the north direction
+    tempCube.position.set(0, 50, indicatorDistance);
+    scene.add(tempCube);
+
+    // ====== load data and create map ======
     const points = [];
     for (const item of data) {
-        const xPosition = item["X_Position"];
-        const yPosition = item["Y_Position"];
-        points.push(new THREE.Vector3(xPosition, 1, yPosition))
+      const xPosition = item["X_Position"];
+      const yPosition = item["Y_Position"];
+      points.push(new THREE.Vector3(xPosition, 1, yPosition));
     }
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
-    const material = new THREE.LineBasicMaterial( {
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const material = new THREE.LineBasicMaterial({
       color: 0x0000ff,
       linewidth: 1,
-      linecap: 'round', //ignored by WebGLRenderer
-      linejoin:  'round' //ignored by WebGLRenderer
-    } );
-    const line = new THREE.Line(
-        geometry,
-        material
-    );
+      linecap: "round", //ignored by WebGLRenderer
+      linejoin: "round", //ignored by WebGLRenderer
+    });
+    const line = new THREE.Line(geometry, material);
     scene.add(line);
 
     // Start the animation loop
     animate();
+    calculateIntervals();
+    moveBoat();
   }
 
   // Function to handle window resize
@@ -216,6 +227,7 @@ const Replay = ({ canvasRef, upperHalfRef, mapRef }) => {
   function animate() {
     requestAnimationFrame(animate);
     render();
+    boat.update();
     // stats.update();
     controls.update();
   }
@@ -226,6 +238,47 @@ const Replay = ({ canvasRef, upperHalfRef, mapRef }) => {
 
     water.material.uniforms["time"].value += 1.0 / 60.0;
     renderer.render(scene, camera);
+  }
+
+  // Calculate the time in Milliseconds between each time record point
+  function calculateIntervals() {
+    const data = timeAndXYData;
+
+    //Retrieve intervals
+    for (let i = 2; i < data.length; i++) {
+      const interval = data[i].time - data[i - 1].time;
+      // console.log(interval);
+      timeIntervals.push(interval * 60 * 1000);
+    }
+  }
+
+  let timeIndex = 0;
+
+  //Recurring function - render position at certain intervals
+  function moveBoat() {
+    const currentInterval = timeIntervals[timeIndex];
+    timeIndex++;
+
+    if (timeIndex < timeIntervals.length) {
+      const data = timeAndXYData;
+      const currentPosition = data[timeIndex];
+      // boat.boat.position.x = currentPosition.X_Position;
+      // boat.boat.position.z = currentPosition.Y_Position;
+
+      console.log("Interval", currentInterval);
+      // boat.position.set(
+      //   currentPosition.X_Position,
+      //   1,
+      //   currentPosition.Y_Position
+      // );
+      tempCube.position.set(
+        currentPosition.X_Position,
+        1,
+        currentPosition.Y_Position
+      );
+
+      setTimeout(moveBoat, currentInterval);
+    }
   }
 
   return <div></div>;
