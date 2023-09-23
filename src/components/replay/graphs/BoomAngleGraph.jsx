37cdacf4b5c91f016613a1040042b828444f17b2
AdungNguyen2509 <adungg2509@gmail.com>
2023-09-23 16:48:20 +1000

    added graphs and control panel, component to process data from csv

---
+++ src/components/replay/graphs/BoomAngleGraph.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import Plot from 'react-plotly.js';
+
+const BoomAngleGraph = ({ time, boomAngle, isRunning, frameIndex, setFrameIndex }) => {
+    useEffect(() => {
+        if (isRunning && frameIndex < time.length) {
+            const timeDifference = time[frameIndex + 1] - time[frameIndex];
+            const animationTimeout = setTimeout(() => {
+            setFrameIndex(frameIndex + 1);
+          }, timeDifference * 1000);
+    
+          return () => clearTimeout(animationTimeout);
+        }
+      }, [isRunning, frameIndex, time]);
+
+    return (
+        <div>
+            <Plot
+                data={[{ 
+                    x: time.slice(0, frameIndex + 1), 
+                    y: boomAngle.slice(0, frameIndex + 1), 
+                    type: 'scatter', 
+                    marker: { color: 'green' } 
+                }]}
+                layout={{ 
+                    width: 350, 
+                    height: 350, 
+                    title: 'Boom Angle', 
+                    xaxis: { title: 'Time' }, 
+                    yaxis: { title: 'Boom Angle' }
+                }}
+            />
+        </div>
+    );
+};
+
+export default BoomAngleGraph;
---
+++ src/components/replay/graphs/GraphControlPanel.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { useCSVReader } from 'react-papaparse';
+
+import "../../../main.css";
+
+import BoomAngleGraph from './BoomAngleGraph';
+import FwdVelocityGraph from './FwdVelocityGraph';
+import HeadingGraph from './HeadingGraph';
+import HeelAngleGraph from './HeelAngleGraph';
+import HikingEffortGraph from './HikingEffortGraph';
+import RudderAngleGraph from './RudderAngleGraph';
+import WindVelocityGraph from './WindVelocityGraph';
+
+const GraphControlPanel = () => {
+    const [boomAngle, setBoomAngle] = useState([]);
+    const [fwdVelo, setFwdVelo] = useState([]);
+    const [heelAngle, setHeelAngle] = useState([]);
+    const [heading, setHeading] = useState([]);
+    const [hiking, setHiking] = useState([]);
+    const [rudderAngle, setRudderAngle] = useState([]);
+    const [time, setTime] = useState([]);
+    const [windVelo, setWindVelo] = useState([]);
+    const [isRunning, setIsRunning] = useState(false);
+    const [frameIndex, setFrameIndex] = useState(0);
+
+    const { CSVReader } = useCSVReader();
+
+    const handleData = result => {
+        setBoomAngle(result.data.slice(2).map(row => row[8]));
+        setFwdVelo(result.data.slice(2).map(row => row[3]));
+        setHeelAngle(result.data.slice(2).map(row => row[10]));
+        setHeading(result.data.slice(2).map(row => row[7]));
+        setHiking(result.data.slice(2).map(row => row[6]));
+        setRudderAngle(result.data.slice(2).map(row => row[9]));
+        setTime(result.data.slice(2).map(row => row[0]));
+        setWindVelo(result.data.slice(2).map(row => row[5]));
+        setIsRunning(true);
+    };
+  
+    return (
+        <div className="graph_panel">
+             <CSVReader onUploadAccepted={handleData}>
+                {({ getRootProps, acceptedFile, getRemoveFileProps }) => 
+                <div>
+                        <button type='button' {...getRootProps()}>Browse file</button>
+                        <div>
+                            {console.log(getRootProps())}
+                        {acceptedFile && acceptedFile.name} 
+                        </div>
+                        <button {...getRemoveFileProps()}>Remove</button>
+                    </div>
+                }
+            </CSVReader>
+            <BoomAngleGraph time={time} boomAngle={boomAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <FwdVelocityGraph time={time} fwdVelo={fwdVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <HeadingGraph time={time} heading={heading} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <HeelAngleGraph time={time} heelAngle={heelAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <HikingEffortGraph time={time} hiking={hiking} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <RudderAngleGraph time={time} rudderAngle={rudderAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+            <WindVelocityGraph time={time} windVelo={windVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />            
+        </div>
+    );
+};
+
+export default GraphControlPanel;
