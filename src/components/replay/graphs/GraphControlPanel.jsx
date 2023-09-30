852c3379d5e74ee855319ed6e28ba85c90101c6a
Bread-John <mr.admin@johnnybread.com>
2023-09-30 18:25:30 +1000

    graph toggles completed

--- src/components/replay/graphs/GraphControlPanel.jsx
+++ src/components/replay/graphs/GraphControlPanel.jsx
@@ -11,6 +11,8 @@ import HikingEffortGraph from './HikingEffortGraph';
 import RudderAngleGraph from './RudderAngleGraph';
 import WindVelocityGraph from './WindVelocityGraph';
 
+const GraphOptions = ['BOOM_ANGLE', 'FWD_VELOCITY', 'HEADING', 'HEEL_ANGLE', 'HIKING_EFFORT', 'RUDDER_ANGLE', 'WIND_VELOCITY'];
+
 const GraphControlPanel = () => {
     const [boomAngle, setBoomAngle] = useState([]);
     const [fwdVelo, setFwdVelo] = useState([]);
@@ -22,7 +24,13 @@ const GraphControlPanel = () => {
     const [windVelo, setWindVelo] = useState([]);
     const [isRunning, setIsRunning] = useState(false);
     const [frameIndex, setFrameIndex] = useState(0);
-    const [plotsDispState, togglePlotsDispState] = useReducer((_plotsDispState, plotsDispStateChange) => _plotsDispState.includes(plotsDispStateChange.type) ? _plotsDispState.filter(s => s !== plotsDispStateChange.type) : [..._plotsDispState.length === 4 ? _plotsDispState.slice(1) : _plotsDispState, plotsDispStateChange.type], ['BOOM_ANGLE', 'FWD_VELOCITY', 'HEADING', 'HEEL_ANGLE', 'HIKING_EFFORT', 'RUDDER_ANGLE', 'WIND_VELOCITY']);
+    const [plotsDispState, togglePlotsDispState] = useReducer(
+        (_plotsDispState, plotsDispStateChange) =>
+            _plotsDispState.includes(plotsDispStateChange.type)
+                ? _plotsDispState.filter(s => s !== plotsDispStateChange.type)
+                : [..._plotsDispState.length === 2 ? _plotsDispState.slice(1) : _plotsDispState, plotsDispStateChange.type],
+        ['FWD_VELOCITY', 'WIND_VELOCITY']
+    );
 
     const { CSVReader } = useCSVReader();
 
@@ -42,25 +50,30 @@ const GraphControlPanel = () => {
         <div className="graph_panel">
              <CSVReader onUploadAccepted={handleData}>
                 {({ getRootProps, acceptedFile, getRemoveFileProps }) => 
-                <div>
-                        <button type='button' {...getRootProps()}>Browse file</button>
-                        <div>
-                            {console.log(getRootProps())}
-                        {acceptedFile && acceptedFile.name} 
-                        </div>
-                        <button {...getRemoveFileProps()}>Remove</button>
+                    <div>
+                        <button type="button" {...getRootProps()}>Browse file</button>
+                        <p>{acceptedFile && acceptedFile.name}</p>
+                        <button type="button" {...getRemoveFileProps()}>Remove</button>
                     </div>
                 }
             </CSVReader>
+            <div>
+                { GraphOptions.map(option =>
+                    <p style={{ color: `white` }}>
+                        <input type={`checkbox`} name={option} checked={plotsDispState.includes(option)} onChange={event => togglePlotsDispState({ type: event.target.name })} />
+                        {option}
+                    </p>
+                ) }
+            </div>
             { plotsDispState.map(dispState =>
                 dispState === 'BOOM_ANGLE' && <BoomAngleGraph time={time} boomAngle={boomAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'FWD_VELOCITY' && <FwdVelocityGraph time={time} fwdVelo={fwdVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'HEADING' && <HeadingGraph time={time} heading={heading} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'HEEL_ANGLE' && <HeelAngleGraph time={time} heelAngle={heelAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'HIKING_EFFORT' && <HikingEffortGraph time={time} hiking={hiking} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'RUDDER_ANGLE' && <RudderAngleGraph time={time} rudderAngle={rudderAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
+                || dispState === 'WIND_VELOCITY' && <WindVelocityGraph time={time} windVelo={windVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
             ) }
-            <FwdVelocityGraph time={time} fwdVelo={fwdVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
-            <HeadingGraph time={time} heading={heading} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
-            <HeelAngleGraph time={time} heelAngle={heelAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
-            <HikingEffortGraph time={time} hiking={hiking} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
-            <RudderAngleGraph time={time} rudderAngle={rudderAngle} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />
-            <WindVelocityGraph time={time} windVelo={windVelo} isRunning={isRunning} frameIndex={frameIndex} setFrameIndex={setFrameIndex} />            
         </div>
     );
 };
